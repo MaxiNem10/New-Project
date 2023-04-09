9461e25dacd67adb12548ae7d08af80dae2880eb
MaxiNem10 <maxinem190790@gmail.com>
2023-04-09 22:17:09 +0800

    homework 11 fix

---
+++ test homeowrk 11 module/scripts/name.js
@@ -0,0 +1,11 @@
+//export let name = 'Max';
+
+export function naimenshee (a, b) {
+    if (a < b) {
+      return a;
+    } else {
+      return b;
+    }
+  }
+  //export let vivod = naimenshee (a, b);
+  //console.log(function)
